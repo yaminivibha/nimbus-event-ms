4ee3a59b691bcdb2ec51827c221dfe5b09a5cb8b
Yamini Ananth <75443071+yva2002@users.noreply.github.com>
2022-12-23 13:55:48 -0500

    adding stuff to zip up lambda function for s3

--- lambda_handler.py
+++ lambda_handler.py
@@ -11,17 +11,17 @@ def lambda_handler(event, context):
     from_email='nimbuseventupdates@gmail.com',
     to_emails=to_emails,
     subject='An Event You Signed Up For Has Changed!',
-    html_content='<strong>Log Into Nimbus to see the event change details!</strong>')
+    html_content='<strong>Log Into Nimbus to see the event change details!</strong>', is_multiple=True)
 
-    sg = SendGridAPIClient('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')
+    sg = SendGridAPIClient(apikey="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")
     
     try:
-        sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
         response = sg.send(message)
         print(response.status_code)
         print(response.body)
         print(response.headers)
+        return response
     except Exception as e:
-        print(e.message)
+        return e
     
-    
+
